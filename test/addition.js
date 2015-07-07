ecfd268962913e7cd0d978d5f647c6e42d500526
Damian Karpiński <d.karpinski@madkom.pl>
2015-07-07 13:59:20 +0200

    zmiana testu

--- test/addition.js
+++ test/addition.js
@@ -3,7 +3,7 @@
 describe('addition', function () {
     it('should add 1+1 correctly', function (done) {
         var onePlusOne = 1 + 1;
-        onePlusOne.should.equal(2);
+        onePlusOne.should.equal(1);
         // must call done() so that mocha know that we are... done.
         // Useful for async tests.
         done();
